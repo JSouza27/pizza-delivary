e898088f1ff2381590569de53db4daa199658cc4
Jonathan Souza <souzajonathan570@gmail.com>
2022-09-12 11:14:43 -0300

    feat: add deployment with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './configs/typeorm.config';
+import { typeOrmConfigAsync } from './configs/typeorm.config';
 import { OrderModule } from './modules/order/order.module';
 import { PizzasModule } from './modules/pizzas/pizzas.module';
 import { OrderItemModule } from './modules/order-item/order-item.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     OrderModule,
     PizzasModule,
     OrderItemModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
 })
 export class AppModule {}
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,13 +1,23 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5434,
-  username: 'root',
-  password: 'pgpassword',
-  database: 'pizza_delivery_db',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
-  autoLoadEntities: true,
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (): Promise<TypeOrmModuleOptions> => {
+    return {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: process.env.DB_SYNC == 'true',
+      autoLoadEntities: true,
+    };
+  },
 };
